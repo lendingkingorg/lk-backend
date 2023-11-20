aa6495d3eb785331b0e3efead487df454d21fe28
Shubham Sidhartha <shubham.sidhartha@lendingking.in>
2023-11-20 20:49:21 +0530

    Initial Repo Commit

---
+++ src/main/java/com/lkbackend/lkbackend/LkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lkbackend.lkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LkBackendApplication.class, args);
+	}
+
+}
