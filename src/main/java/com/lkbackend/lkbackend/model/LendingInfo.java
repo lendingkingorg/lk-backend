de30a9e7370f4766930f0e05b4bd4ce708048ced
Mitaligrawal <agrawalmitali05@gmail.com>
2023-12-26 18:46:41 +0530

    database

--- src/main/java/com/lkbackend/lkbackend/model/LendingInfo.java
+++ src/main/java/com/lkbackend/lkbackend/model/LendingInfo.java
@@ -1,10 +1,7 @@
 package com.lkbackend.lkbackend.model;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -14,6 +11,7 @@ public class LendingInfo {
 
     @Id
     @Column(name = "userId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
 
     private long mobileNumber;
