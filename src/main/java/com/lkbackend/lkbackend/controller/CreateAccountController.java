1315f4c0c738f8ea0ee16ce2bf59dad74f8da13e
Mitaligrawal <agrawalmitali05@gmail.com>
2024-01-31 02:09:05 +0530

    Welcome Email and forgot mpin template added

--- src/main/java/com/lkbackend/lkbackend/controller/CreateAccountController.java
+++ src/main/java/com/lkbackend/lkbackend/controller/CreateAccountController.java
@@ -4,10 +4,12 @@ import com.lkbackend.lkbackend.Entity.CustomerResponseAccountCreate;
 import com.lkbackend.lkbackend.Entity.GenerateReferralCode;
 import com.lkbackend.lkbackend.Service.LendingInfoService;
 import com.lkbackend.lkbackend.model.LendingInfo;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping
@@ -34,6 +36,54 @@ public class CreateAccountController {
 
 
         lendingInfoService.save(lendingInfo);
+
+        LendingInfo user_info = lendingInfoService.findByMobileNumber(mobile);
+
+
+        // Set the API endpoint URL
+        String apiUrl = "https://control.msg91.com/api/v5/email/send";
+
+        // Create JSON body as a string
+        String jsonBody = "{\n" +
+                "  \"recipients\": [\n" +
+                "    {\n" +
+                "      \"to\": [\n" +
+                "        {\n" +
+                "          \"name\": \"" + user_info.getName() + "\",\n" +
+                "          \"email\": \"" + user_info.getEmail() + "\"\n" +
+                "        }\n" +
+                "      ],\n" +
+                "      \"variables\": {\n" +
+                "        \"MPIN\": \"" + user_info.getmPin() + "\",\n" +
+                "        \"USER\": \"" + user_info.getName() + "\"\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"from\": {\n" +
+                "    \"name\": \"Support\",\n" +
+                "    \"email\": \"support@mail.lendingking.in\"\n" +
+                "  },\n" +
+                "  \"domain\": \"mail.lendingking.in\",\n" +
+                "  \"template_id\": \"welcome_template_2\"\n" +
+                "}";
+
+        // Create an HTTP client
+        String authKey = "410480ArZD05k4xV6566f67eP1";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("authkey", authKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String mob = Long.toString(mobile);
+
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(jsonBody, headers);
+
+        ResponseEntity<String> responseEntity = new RestTemplate().exchange(
+                apiUrl,
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        );
 
         CustomerResponseAccountCreate customerResponseAccountCreate = new CustomerResponseAccountCreate();
         CustomerResponseAccountCreate.Data data = new CustomerResponseAccountCreate.Data();
