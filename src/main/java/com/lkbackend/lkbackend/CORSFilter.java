1658578bce77e5a44e84d9ff93c57f2042c41664
Akshay <akshaykumar.dadeech@lendingking.in>
2024-02-25 18:47:25 +0530

    added cors filter

---
+++ src/main/java/com/lkbackend/lkbackend/CORSFilter.java
@@ -0,0 +1,44 @@
+package com.lkbackend.lkbackend;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
+            ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "access_token, authorization, content-type");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+
+    @Override
+    public void destroy() {
+        //
+    }
+
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+        //
+    }
+}
